3a8aef6f6775868e1dea57523ace9f4622fdc036
Andrew J Daniel <adaniel@uk.ibm.com>
2018-06-14 19:50:13 +0100

    chore: Style tweaks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 
 import Home from './App/components/home';
@@ -11,6 +11,10 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <h1 className="App-title">Hursley Fit52</h1>
+            <p className="App-links">
+              <Link to="/">Home</Link>
+              <Link to="/login">Login</Link>
+            </p>
           </header>
           <section className="App-content">
             <Route exact path="/" component={Home} />
