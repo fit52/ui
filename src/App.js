6da31ec5d4ac291676f38d095da08e443200c5ae
Andrew Daniel <andrewjd1@gmail.com>
2018-08-02 19:30:53 +0100

    precommit message

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Route, Link, Switch,
+} from 'react-router-dom';
 import './App.css';
 
 import Home from './App/components/Home';
