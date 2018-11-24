326a1693e4972b0f3a614c814f6ee16383b5e127
Andrew Daniel <andrewjd1@gmail.com>
2018-11-24 09:51:43 +0000

    feat: Add icons and Events back button

--- src/App/components/Event.js
+++ src/App/components/Event.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
-import Spinner from './Spinner';
+import { Link } from 'react-router-dom';
+import { MdArrowBack } from 'react-icons/md';
 
 import api from '../services/api';
+import Spinner from './Spinner';
 
 export default class Events extends React.Component {
   static propTypes = {
@@ -18,7 +20,13 @@ export default class Events extends React.Component {
   componentDidMount() {
     const { match } = this.props;
     api.getEvent(match.params.eventId)
-      .then(event => this.setState({ event, loading: false }));
+      .then((eventData) => {
+        const event = {
+          ...eventData,
+          dateString: moment(eventData.date).format('MMMM Do YYYY'),
+        };
+        this.setState({ event, loading: false });
+      });
   }
 
   render() {
@@ -29,7 +37,8 @@ export default class Events extends React.Component {
         <Spinner loading={loading} />
         {event && (
           <div className="Event">
-            <h3>{moment(event.date).format('MMMM Do YYYY')}</h3>
+            <p><Link to="/events"><MdArrowBack /><span>Events</span></Link></p>
+            <h3>{event.dateString}</h3>
 
             <h3>Stats</h3>
             <p>Number of 2K runners: {event.counts.twok}</p>
@@ -39,7 +48,7 @@ export default class Events extends React.Component {
 
             <h3>Results</h3>
             {event.results.map(result => (
-              <React.Fragment>
+              <React.Fragment key={result.name}>
                 <h3>{result.name} {result.firstEvent && '(first time)'}</h3>
                 <p>Position: {result.pos}</p>
                 <p>Time: {result.time}</p>
