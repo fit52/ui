7f58e4995eb8c0e4ff2033bd57c8b1286d07f5c1
Andrew J Daniel <adaniel@uk.ibm.com>
2019-03-06 17:00:56 +0000

    feat: Wordpress page caching

--- server/site.js
+++ server/site.js
@@ -1,24 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const wpcom = require('wpcom')(/* auth token */);
+const Cache = require('./utils/cache');
 
 const router = express.Router();
 const blog = wpcom.site('hursleyfit52.wordpress.com');
+const cache = new Cache();
 
 router.use(bodyParser.json());
 
-const cache = {};
-const ONE_DAY = 24 * 60 * 60 * 1000;
-
 router.get('/posts', async (req, res) => {
-  if (!cache.posts || Date.now() - cache.posts.timestamp > ONE_DAY) {
-    cache.posts = {
-      data: await blog.postsList({ number: 10 }),
-      timestamp: Date.now(),
-    };
-  }
+  const posts = await cache.get('posts', blog.postsList({ number: 10 }));
 
-  return res.json(cache.posts.data.posts.map(post => ({
+  return res.json(posts.data.posts.map(post => ({
     id: post.ID,
     author: {
       firstName: post.author.first_name,
@@ -33,8 +27,8 @@ router.get('/posts', async (req, res) => {
 router.get('/page/:id', async (req, res) => {
   try {
     /* eslint-disable camelcase */
-    console.log('Getting page', req.params.id);
-    const data = await blog.post({ slug: req.params.id }).get();
+    const { id } = req.params;
+    const data = await cache.get(`Page_${id}`, () => blog.post({ slug: id }).get());
     const { title, content, post_thumbnail } = data;
     const pictureUrl = post_thumbnail ? post_thumbnail.URL : undefined;
     return res.json({ title, content, pictureUrl });
